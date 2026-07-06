d8e2b81fac5fc0ab1eb11d5c3fbf27173d256ee7
mahdibahraini1386mcs-spec <mahdibahraini1386mcs@gmail.com>
2026-07-06 17:25:15 +0330

    Refactor: Update HUD to render persistent High Score

--- src/GameHUD.java
+++ src/GameHUD.java
@@ -1,19 +1,18 @@
 import java.awt.*;
 
 public class GameHUD {
-    public static void draw(Graphics2D g2d, int score, int lives, int level) {
+    public static void draw(Graphics2D g2d, int score, int highScore, int lives, int level) {
         g2d.setColor(Color.WHITE);
         g2d.setFont(new Font("SansSerif", Font.BOLD, 18));
 
-        // نمایش اطلاعات در گوشه بالا سمت چپ
         g2d.drawString("Level: " + level, 20, 30);
         g2d.drawString("Score: " + score, 20, 60);
-        g2d.drawString("Lives: ", 20, 90);
+        g2d.drawString("High Score: " + highScore, 20, 90);
+        g2d.drawString("Lives: ", 20, 120);
 
-        // رسم قلب برای جان‌ها
         g2d.setColor(Color.RED);
         for (int i = 0; i < lives; i++) {
-            g2d.fillOval(80 + (i * 30), 75, 20, 20);
+            g2d.fillOval(80 + (i * 30), 105, 20, 20);
         }
     }
 }
