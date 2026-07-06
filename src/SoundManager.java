bfdbd8c74393d7ab19949fba2bc61c797210d00e
mahdibahraini1386mcs-spec <mahdibahraini1386mcs@gmail.com>
2026-07-06 13:52:11 +0330

    Feat: Add SoundManager utility class for audio playback

---
+++ src/SoundManager.java
@@ -0,0 +1,25 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SoundManager {
+
+    public static void playSound(String fileName) {
+        try {
+            
+            String path = "resources/sounds/sound-effects/" + fileName;
+            File soundFile = new File(path);
+
+            if (soundFile.exists()) {
+                AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioIn);
+                clip.start();
+            } else {
+                System.err.println("Sound file not found: " + path);
+            }
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.err.println("Error playing sound: " + fileName);
+        }
+    }
+}
